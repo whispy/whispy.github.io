8d97676ad05bda9029162fd5194e9a8d80403ba4
Whispy <dan.berkowitz1@gmail.com>
2014-03-30 14:55:23 -0400

    Fixed custom.js loading + other stuff

--- js/custom.js
+++ js/custom.js
@@ -12,11 +12,10 @@ bindListeners(menuItems); // call the function 'bindListeners' with the variable
 
 function loadContent() { // Inline loading of content
 var toLoad = jQuery(this).attr('href')+' .content';
-//window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5); //append url
-$('#about').load(toLoad); // select the element with id 'about' and load it with data returned from the function 'toLoad' (currently the 'toLoad' function doesn't exist in this context)
-$('#about').show(); // display/show the element with id 'about'
-}
-
+window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5); //append url
+$('.panel').load(toLoad); // select the element with class 'panel' and load it with data returned from the function 'toLoad'
+$('.panel').fadeIn(); // display the element with class 'panel'
+} // Inline loading of content end
  
 function bindListeners(menuItems) { // define the function 'bindListeners' that takes a single argument
  
@@ -27,9 +26,17 @@ function bindListeners(menuItems) { // define the function 'bindListeners' that 
 			menuItems.children().removeClass('on'); // remove the 'on' class for all elements inside all menuItems
 			$(e.target).addClass('on'); // add the class 'on' to the element we just clicked on
 			$('#header').animate({"margin-left":"6%"}); // animate the header shifting to the left
-			loadContent(); // call the function 'loadContent' (currently the 'loadContent' function doesn't exist in this context)
+			loadContent.call(this); // call the function 'loadContent'
  			return false;	// Inline loading of content end
 		}); // #+id click event handler
+		//Clicking on Home or logo
+	 $("#logo").click(function() {
+	 		menuItems.children().removeClass('on');
+			$(".header").animate({
+	 			"margin-left":"37.5%",
+	 		}); //animate method end
+			$(".panel").fadeOut(150);
+	}); // click method end
 	}); // menuItems.each
 } // bindListeners
  
@@ -43,158 +50,16 @@ return {
 $(document).ready(function() { // when the DOM is fully loaded, execute the contents of this anonymous function
  	var navSelector = '#menu li'; // set the variable 'navSelector' to the all 'li' elements inside the element with id 'menu'
  	fancyDan.init(navSelector); // call the function 'init' on the module 'fancyDan' with navSelector as an argument to the function 'init'
+
+ 	var hash = window.location.hash.substr(1);
+	var href = $('#menu li a').each(function(){
+    var href = $(this).attr('href');
+    if(hash==href.substr(0,href.length-5)){
+        var toLoad = hash+'.html .content';
+        $('.content').load(toLoad)
+    }
+});
 }); // $(document).ready
-
-
-
-/*
-jQuery(document).ready(function() {
-	var aboutA = jQuery("#about_a");
-	var workA = jQuery("#work_a");
-	var contactA = jQuery("#contact_a");
-	var home = jQuery("#home_a");
-	var allNavA = jQuery("#about_a, #work_a, #contact_a")
-	var container = jQuery(".container");
-	var width = $(window).width();
-
-//Desktop navigation
-
-	//Clicking on About
-	 jQuery("#about_a").click(function() {
-		var toLoad = jQuery(this).attr('href')+' .content';
-		if(width<=777) {
-  			container.addClass('mobileNav'); //why is this not working?
-  		} // if end
-  		else {	
-		 if (!aboutA.is('.on')) {
-			aboutA.addClass('on');
-		 	jQuery(".header").animate({
-		 	"margin-left":"6%",
-		 	}); //animate method end
-		 	jQuery("#about").fadeIn("slow");
-		 	jQuery("#contact, #work").hide();
-		 	window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5); //append url
-			
-			function loadContent() {	// Inline loading of content
-	       	 jQuery('#about').load(toLoad)
-	       	 jQuery('#about').show();
-	    	}	
-	    	loadContent();
-	    	return false;	// Inline loading of content end
-		 } else {
-		 	aboutA.removeClass('on');
-		 	jQuery("#contact, #work").hide();
-		 	jQuery("#about").fadeIn("slow");
-		 	window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5); //append url
-			
-			function loadContent() {	// Inline loading of content
-	       	 jQuery('#about').load(toLoad)
-	       	 jQuery('#about').show();
-	    	}	
-	    	loadContent();
-	    	return false;	// Inline loading of content end
-			} // else end
-		} //else end
-	}); // click method end
-
-	 //Clicking on Work
-	 jQuery("#work_a").click(function() {
-	 	var toLoad = jQuery(this).attr('href')+' .content';
-		if(width<=777) {
-  			container.addClass('mobileNav'); //why is this not working?
-  		} // if end
-  		else {	
-		 	if (!workA.is('.on')) {
-				workA.addClass('on');
-		 		jQuery(".header").animate({
-		 		"margin-left":"6%",
-		 	}); //animate method end
-		 		jQuery("#work").fadeIn("slow");
-		 		jQuery("#contact, #about").hide();
-		 	window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5); //append url
-			
-			function loadContent() { // Inline loading of content
-	       	 jQuery('#work').load(toLoad)
-	       	 jQuery('#work').show();
-	    	}	
-	    	loadContent();
-	    	return false;	// Inline loading of content end
-
-		 } else {
-		 	workA.removeClass('on');
-		 	jQuery("#contact, #about").hide();
-		 	jQuery("#work").fadeIn("slow");
-		 	window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5); //append url
-			
-			function loadContent() { // Inline loading of content
-	       	 jQuery('#work').load(toLoad)
-	       	 jQuery('#work').show();
-	    	}	
-	    	loadContent();
-	    	return false;	// Inline loading of content end
-		} // else end
-	} //else end
-	}); // click method end
-
-	 //Clicking on Contact
-	 jQuery("#contact_a").click(function() {
-	 	var toLoad = jQuery(this).attr('href')+' .content';
-	 	if(width<=777) {
-  			container.addClass('mobileNav'); //why is this not working?
-  		} // if end
-  		else {	
-		 	if (!contactA.is('.on')) {
-				contactA.addClass('on');
-		 		jQuery(".header").animate({
-		 		"margin-left":"6%",
-		 	}); //animate method end
-		 		jQuery("#contact").fadeIn("slow");
-		 		jQuery("#work, #about").hide();
-			window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5); //append url
-		 	
-		 	function loadContent() { // Inline loading of content
-	       	 jQuery('#contact').load(toLoad)
-	       	 jQuery('#contact').show();
-	    	}	
-	    	loadContent();
-	    	return false;	// Inline loading of content end
-		 } else {
-		 	contactA.removeClass('on');
-		 	jQuery("#work, #about").hide();
-		 	jQuery("#contact").fadeIn("slow");
-		 	window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5); //append url
-		 	
-		 	function loadContent() { // Inline loading of content
-	       	 jQuery('#contact').load(toLoad)
-	       	 jQuery('#contact').show();
-	    	}	
-	    	loadContent();
-	    	return false;	// Inline loading of content end
-			} // else end
-		} //else end
-	}); // click method end
-
-	 //Clicking on Home or logo
-	 jQuery("#home_a, #logo").click(function() {
-
-	 	if (!home.is('.on')) {
-			home.addClass('on');
-	 		jQuery(".header").animate({
-	 		"margin-left":"37.5%",
-	 	}); //animate method end
-	 		jQuery("#about, #work, #contact").hide();
-			allNavA.removeClass('on');
-	 } else {
-	 		home.removeClass('on');
-			jQuery(".header").animate({
-	 		"margin-left":"37.5%",
-	 	}); //animate method end
-			jQuery("#about, #work, #contact").hide();
-			allNavA.removeClass('on');
-		} // else end
-	}); // click method end
-//Desktop navigation end
-
 
 //Get window size
 function checkSize() {
@@ -204,7 +69,6 @@ function checkSize() {
   	container.addClass('mobileNav');
   } // if end
   else {
-  	jQuery("#home_a").show();
   	container.removeClass('mobileNav');
   	container.css('margin-left','');
   } //else end
@@ -215,10 +79,7 @@ $(window).resize(function() {
   checkSize();
 });
 //Get window size end
-*/
 /*
-
-
 //Animations for mobile navigation
  jQuery("#menu_a").click(function() {
 
@@ -231,7 +92,7 @@ $(window).resize(function() {
 	 		jQuery(".header").animate({
 	 		"margin-left":"61%",
 	 	}); //animate method end
-	 		/*jQuery(".container").animate({
+	 		jQuery(".container").animate({
 	 		"margin-left":"75px"
 	 	}) // animate method end
 
@@ -239,7 +100,7 @@ $(window).resize(function() {
 	 else {
 	 	jQuery("#about, #work, #contact, #home_a").fadeOut("fast");
 		container.removeClass('mobileNav');
-		/*jQuery(".container").animate({
+		jQuery(".container").animate({
 	 		"margin-left":""
 	 	}) // animate method end
 	 		jQuery(".header").animate({
@@ -249,14 +110,4 @@ $(window).resize(function() {
 	}); // click method end
 //Animations for mobile navigation end
 
-}); //ready method end
-
-	// Code to get linking to work somehow
-	/*var hash = window.location.hash.substr(1);
-	var href = $('#about_a').each(function(){
-    	var href = $(this).attr('href');
-    	if(hash==href.substr(0,href.length-5)){
-        	var toLoad = hash+'.html #content';
-        	$('#content').load(toLoad)
-    	}
-	}); */
+*/
