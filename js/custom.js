1edb95884fae95c8b4552ce79d1b93d018b6f335
whispy <whispy@users.noreply.github.com>
2014-03-31 15:09:09 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -88,9 +88,9 @@ $(document).ready(function () { // when the DOM is fully loaded, execute the con
     
     function appendWork(data) {
     	// find the content you need from the "full html"
-    	var work = $("<div>").append(data).find("#workThumbBG .content").contents()
+    	var work = $("<div>").append(data).find("#workThumbBG .content img")
     	$(".content").html(work);
-    	fancyWork.initThumbs(work.find("div img"));
+    	fancyWork.initThumbs(work);
     	return work;
     }
 
