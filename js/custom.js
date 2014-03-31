cb02ba309393c646cf9facab82f95272aafe8103
whispy <whispy@users.noreply.github.com>
2014-03-31 12:04:04 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -46,7 +46,6 @@ return {
 
 var fancyWork = (function() { // define the globally scoped variable 'fancyWork' and set it equal to this immediately invoked anonymous function expression (http://benalman.com/news/2010/11/immediately-invoked-function-expression/)
         function initThumbs(selectorThumbs) { // define the function 'initThumbs' that takes a single argument
-             var workThumbs = '#workThumbBG div img'  //this is nonexistent on page load
              var workThumbs = $(selectorThumbs); // set the variable 'workThumbs' to all elements that match the contents of variable 'selector' using jQuery
             bindListenersThumbs(workThumbs); // call the function 'bindListeners' with the variable 'menuItems'
         } // initThumbs
@@ -60,7 +59,6 @@ var fancyWork = (function() { // define the globally scoped variable 'fancyWork'
        } // Inline loading of content end
  
    function bindListenersThumbs(workThumbs) { // define the function 'bindListenersThumbs' that takes a single argument
-var workThumbs = '#workThumbBG div img'  //this is nonexistent on page load
  	workThumbs.each(function(key) { // for each of the elements in the variable 'workThumbs' call the anonymous function (generally referred to as a callback) with a single argument that denotes the current index of the for loop (aka the variable 'key')
 	var idThumbs = $(workThumbs[key]).attr('img'); // set the variable 'id' to the first id returned from the first child of all children elements within the current menuItem (which is selected/indexed by the variable 'key') using jQuery
 	alert('hi');
