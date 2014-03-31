1368b503befcd55167103b37a989c24acdd8b366
whispy <whispy@users.noreply.github.com>
2014-03-31 11:34:43 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -59,12 +59,12 @@ var fancyWork = (function() { // define the globally scoped variable 'fancyWork'
        } // Inline loading of content end
  
    function bindListenersThumbs(workThumbs) { // define the function 'bindListenersThumbs' that takes a single argument
- 	alert('hi');
+
  	workThumbs.each(function(key) { // for each of the elements in the variable 'workThumbs' call the anonymous function (generally referred to as a callback) with a single argument that denotes the current index of the for loop (aka the variable 'key')
 	var idThumbs = $(workThumbs[key]).attr('id'); // set the variable 'id' to the first id returned from the first child of all children elements within the current menuItem (which is selected/indexed by the variable 'key') using jQuery
 
 		$('#' + idThumbs).on('click', function(e) { // bind this jQuery click event handler to the element that has the variable 'id' as its identifier (when a click event occurs on an id that is being listened to)
-		
+		 	alert('hi');
 			//menuItems.children().removeClass('on'); // remove the 'on' class for all elements inside all menuItems
 			//$(e.target).addClass('on'); // add the class 'on' to the element we just clicked on
 			$('#workThumbBG').animate({
