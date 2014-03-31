12822f5593e9c789785bd97bbe53d9a93fe1834a
whispy <whispy@users.noreply.github.com>
2014-03-31 15:03:21 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -95,7 +95,7 @@ $(document).ready(function () { // when the DOM is fully loaded, execute the con
     }
 
     function loadWork() {
-    	return $.load("work.html");
+    	return $.get("work.html");
     }
 
     function hashChange() {
