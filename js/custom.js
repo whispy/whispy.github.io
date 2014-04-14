1b573741e4477eca267512399d6c980bdd5dfc81
whispy <whispy@users.noreply.github.com>
2014-04-14 10:41:50 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -165,7 +165,14 @@ $(document).ready(function () { // when the DOM is fully loaded, execute the con
 		currHash = window.location.hash
 		var navSelector = '#menu li';
 		if (currHash === '') {
-			resetDivs();
+					panel.removeClass("showDiv")
+		panel.addClass("resetDiv");
+		panel.css({
+			"width":"",
+			"margin-right":"", // figure out how to do this stuff with classes, if possible
+			"left":"",
+		});
+		workDisplay.addClass(".resetDiv");
 			fancyNav(navSelector); // call the function fancyNav with navSelector as an argument
 		}
 
