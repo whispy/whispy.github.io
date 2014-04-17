be50f43a9e99adfeff962c80588f74c9d4872abc
Whispy <dan.berkowitz1@gmail.com>
2014-04-17 10:24:00 -0400

    stuff

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,8 @@ function fancyNav(selector) {
 	var menuItems = $(selector); // set the variable 'menuItems' to all elements that match the contents of variable 'selector' using jQuery
 	bindListeners(menuItems); // call the function 'bindListeners' with the variable 'menuItems'
 
+
+
 	function resetDivs() {
 		panel.removeClass("showDiv");
 		panel.removeClass("panelSidebar");
@@ -17,7 +19,7 @@ function fancyNav(selector) {
 	
 	function showDivs() {
 		panel.removeClass("resetDiv");
-    		panel.addClass("showDiv")
+    	panel.addClass("showDiv")
 	}; //showDivs end
 
 	loadContent = function(toLoad) { // Inline loading of content
@@ -33,8 +35,8 @@ function fancyNav(selector) {
     	$('.navigation').on('click', 'a', function() {
     		resetDivs(); // fix flashing on this -> try to cache data somehow
         	var toLoad = $(this).attr('href').replace('.html', '');
-        	var headerWrapperWidth = $('#headerWrapper').width();
-        	var panelLeft = $('#indexPanel').offset().left - headerWrapperWidth;
+			var headerWrapperWidth = $('#headerWrapper').width();
+    		var panelLeft = $('#indexPanel').offset().left - headerWrapperWidth;
 
 			if(width>=1400) {
 			$.stylesheet('#headerWrapper.panelLeft').css({
@@ -64,7 +66,7 @@ function fancyNav(selector) {
 	$("#selfieDiv").click(function() {
 		resetDivs();
 		$.stylesheet('#headerWrapper.panelLeft').css({
-			"margin-left":headerLeft + 'px',
+			"margin-left":headerCenter + 'px',
 		});
 		$('#headerWrapper').addClass('panelLeft');
 	}); // click method end
@@ -106,15 +108,12 @@ function fancyWork(selector) {
 					$('.content').animate({
 						"margin-top":"0",
 					}, 300 , "easeOutQuart");
-					$(".panelSidebar").css({
-						"left":panelRight + 110 // scrollbar width 17. problems if going from scrollbar -> no scrollbar (doesn't take into account scrollbar width)
-					});
-					
-					panel.addClass("panelSidebar");
 
-				/*	panel.css({
-						"left":(0 - panelRight) + 100
-					})*/
+					$.stylesheet("#indexPanel.panelSidebar").css({
+						"left":panelRight + 110 + 'px' // scrollbar width 17. problems if going from scrollbar -> no scrollbar (doesn't take into account scrollbar width)
+					});
+		
+					panel.addClass("panelSidebar");
 
 					
 					$('.content .imgDiv').css({
@@ -134,30 +133,26 @@ function fancyWork(selector) {
 }; // fancyWork
 
 function fancyBlog(selector) {
-	var blogPosts = $(selector); // set the variable 'workThumbs' to all elements that match the contents of variable 'selector' using jQuery
-	bindListeners(blogPosts); // call the function 'bindListeners' with the variable 'menuItems'
+	var blogArticles = $(selector); // set the variable 'workThumbs' to all elements that match the contents of variable 'selector' using jQuery
+	bindListeners(blogArticles); // call the function 'bindListeners' with the variable 'menuItems'
 
-	function loadPieces() { // Inline loading of content
-		var toFetch = toLoad + '.html' + ' .work ';
+	function loadArticles(toLoad) { // Inline loading of content
+		var toFetch = toLoad + '.html' + ' .content ';
 		window.location.hash = toLoad
-		panel.load('./articles/article1.html');
-		//workDisplayFadeIn();
+				console.log('pippy');
+				panel.load(toFetch);
+				console.log(toFetch);
+		console.log('trippy');
+
 	} // Inline loading of content end
 
-	function workDisplayFadeIn() {
-		workDisplay.css({
-    		"opacity":"0.0",
-    		"visibility":"visible",
-    	}).animate({
-    		"opacity":"1.0",
-    	}, 300);
-	};
 	
 	function bindListeners() {
-    	$('.content a').on('click', 'a', function() {
-    		//var toLoad = $(this).attr('href').replace('.html', '');
-				loadPieces();
-				return false;
+    	$('.content p').on('click', 'a', function() {
+    		var toLoad = $(this).attr('href').replace('.html', '');
+    		
+			loadArticles(toLoad);
+			return false;
 			
     	}); //click function end
     }; //bindListeners end
@@ -167,22 +162,23 @@ function fancyBlog(selector) {
 
  
 $(document).ready(function () { // when the DOM is fully loaded, execute the contents of this anonymous function
- console.log($.stylesheet('body').rules());
+ //console.log($.stylesheet('body').rules());
 	//Defining global variables
 	width = $(window).width();
 	panel = $('.panel');
 	workDisplay = $('.workDisplay');
 	headerLeft = $('#headerWrapper').offset().left;
+	headerCenter = (width / 2) - 109.5;
 	panelRight = $('#indexWorkDisplay').width();
 
 	
 	$("#headerWrapper").addClass("panelLeft");
 	panel.addClass("resetDiv");
-	console.log(headerLeft);
+	//console.log(headerLeft);
 	$.stylesheet('#headerWrapper.panelLeft').css({
-		"margin-left":headerLeft + 'px',
-    	})
-    	console.log($.stylesheet('#headerWrapper.panelLeft').rules());
+		"margin-left":headerCenter + 'px',
+    })
+    	//console.log($.stylesheet('#headerWrapper.panelLeft').rules());
 
 
 
@@ -196,10 +192,24 @@ $(document).ready(function () { // when the DOM is fully loaded, execute the con
 		return $.get("work.html");
 	}
 
+
+	function setBlogArticles(data) {
+		var blogArticles = '.content p'
+		fancyBlog(blogArticles);
+		return blogArticles;
+	}
+
+	function getBlogArticles() {
+		return $.get("blog.html");
+	}
+
 	window.onhashchange = workHashChange
 	function workHashChange() { // if the hash is not #work and changes to work, this fires
 		if (window.location.hash === '#work') {
 			getWorkThumbs().then(setWorkThumbs);
+		}
+		if (window.location.hash === '#blog') {
+			getBlogArticles().then(setBlogArticles);
 		}
 	};
 
@@ -214,12 +224,7 @@ $(document).ready(function () { // when the DOM is fully loaded, execute the con
 			fancyNav(navSelector); // call the function fancyNav with navSelector as an argument
 		}
 
-		/*if (currHash === '#blog') {
-			console.log('hi');
-			//panel.load('./articles/article1.html');
-			console.log('bye');
-			return false;
-		}*/
+
 
 		var navClick = $('.navigation').find(currHash);
 		if (navClick.length) {
@@ -229,7 +234,7 @@ $(document).ready(function () { // when the DOM is fully loaded, execute the con
 				getWorkThumbs().then(setWorkThumbs);
 			}
 			if (currHash === '#blog') {
-				fancyBlog();
+				getBlogArticles().then(setBlogArticles);
 			}
 		}
 
@@ -259,20 +264,24 @@ function checkSize() {
 	var container = jQuery(".container");
 	currHash = window.location.hash
 
-        var headerWrapperWidth = $('#headerWrapper').width();
-        var panelLeft = $('#indexPanel').offset().left - headerWrapperWidth;
 		var navClick = $('.navigation').find(currHash);
 		if (navClick.length)  {
 			if(width>=1400) {
+				var headerWrapperWidth = $('#headerWrapper').width();
+       			var panelLeft = $('#indexPanel').offset().left - headerWrapperWidth;
+				console.log(panelLeft);
 				$.stylesheet('#headerWrapper.panelLeft').css({
-      				 "margin-left":panelLeft + 'px',
+      				"margin-left":panelLeft + 'px',
    				})
 			}
 
 			if(width<=1399 && width>=778) {
+				var headerWrapperWidth = $('#headerWrapper').width();
+       			var panelLeft = $('#indexPanel').offset().left - headerWrapperWidth;
 				var panelLeft1399 = panelLeft + 40;
+				console.log(panelLeft1399);
 				$.stylesheet('#headerWrapper.panelLeft').css({
-      				 "margin-left":panelLeft1399 + 'px',
+      				"margin-left":panelLeft1399 + 'px',
    				})
 			}
 		}
@@ -288,15 +297,19 @@ function checkSize() {
 //       		"margin-left":""
     	//	})
 //	}
-	else {
-		//container.removeClass('mobileNav');
-		container.css('margin-left','');
+	if(currHash === '') {
+		var headerCenter = (width / 2) - 109.5;
+				$.stylesheet('#headerWrapper.panelLeft').css({
+      				"margin-left":headerCenter + 'px',
+   				})
 	} //else end
+
 }
 
 $(window).resize(function() {
-		
+	$('#headerWrapper').addClass("transitionReset");
 	checkSize();
+	$('#headerWrapper').removeClass("transitionReset");
 }); 
 //Get window size end
 
