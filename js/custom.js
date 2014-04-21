3c56f2e33872e7c688df72a8b661d38854dc67c5
whispy <whispy@users.noreply.github.com>
2014-04-21 14:45:47 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -158,14 +158,14 @@ $(window).on('pronto.request', function(e, eventInfo){
 	fancyNav(navSelector);
 })
 
-$('.imgDiv').on('click', 'a', function() {// request and render are targeted at the window. See if possible to target them more specifically.
-	//$('.imgDiv a').on('pronto.render', function(e, eventInfo){ 
+// request and render are targeted at the window. See if possible to target them more specifically.
+	$('.imgDiv a').on('pronto.render', function(e, eventInfo){ 
 		console.log(window.location.pathname)
 		if(window.location.pathname.indexOf("work") != -1) {
 			imgDivPronto();
 		}
-	//})
-})
+	})
+
 
 
 function imgDivPronto() {
