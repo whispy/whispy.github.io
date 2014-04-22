4eca07d1a54035ae4176445f411a61f09f41b0a1
Whispy <dan.berkowitz1@gmail.com>
2014-04-22 12:39:40 -0400

    removed navselector

--- js/custom.js
+++ js/custom.js
@@ -147,8 +147,8 @@ function fancyBlog(selector) {
 
 // could create a fancyWorkLoad and fancyWorkClick?
 $(window).on('pronto.request', function(){
-	var navSelector = '#menu li';
-	fancyNav(navSelector);
+	//var navSelector = '#menu li';
+	fancyNav();
 })
 
 $(window).on('pronto.request', 'a', function(){
