2787f1a51d72f6dc4b9211fbb704c4a572edc45e
Whispy <dan.berkowitz1@gmail.com>
2014-05-02 18:24:36 -0400

    grunt includes stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,19 @@
 module.exports = function(grunt) {
+
+	grunt.initConfig({
+		includereplace: {
+			your_target: {
+				options: {
+				// Task-specific options go here.
+      			},
+      			files: [
+      				{src: ['*.html', 'articles/*.html', 'pieces/*.html'], dest: '/'}
+      			]
+    		}
+  		}
+	})
+
   grunt.loadNpmTasks('grunt-include-replace');
+
+  grunt.registerTask('default', ['includereplace']);
 };
