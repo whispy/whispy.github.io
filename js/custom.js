9db4a584c5eaaf35498eadf5890d446962a4f649
Whispy <dan.berkowitz1@gmail.com>
2014-07-30 11:12:35 -0400

    switched designs/writings in nav

--- js/custom.js
+++ js/custom.js
@@ -27,7 +27,8 @@ function fancyNav(selector) {
 	} // loadContent end
 
 	function bindListeners() {
-		if((width >= 778)){
+		if((width >= 778) && ((window.location.href.indexOf("index") == -1) || (!window.location.pathname === '/'))){
+
 			$('#headerWrapper').addClass('headerLeft');
 		}
         divInit();
@@ -211,7 +212,11 @@ function checkSize() {
 	var container = jQuery(".container");
 
 	//add headerLeft if browser is resized to be >778 px AND page is not 'index' or '/'
-	if((width >= 778) && ((window.location.href.indexOf("index") == -1) || (!window.location.pathname === '/'))){
+
+	/* I dunno. */
+	if((width >= 778) && ((window.location.href.indexOf("index") == false) || (!window.location.pathname === '/'))){
+		console.log((window.location.href.indexOf("index") == -1));
+		console.log((!window.location.pathname === '/'));
 		$('#headerWrapper').addClass('headerLeft');
 	}
 
