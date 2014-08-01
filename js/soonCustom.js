2e1efec8c15b945eaf13573dcf2ddfc1a963b0c7
Whispy <dan.berkowitz1@gmail.com>
2014-08-01 16:30:41 -0400

    cleaned up some unused stuff

--- js/soonCustom.js
+++
@@ -1,8 +0,0 @@
-$("h2").each(function() {
-  var wordArray = $(this).text().split(" ");
-  if (wordArray.length > 1) {
-    wordArray[wordArray.length-2] += "&nbsp;" + wordArray[wordArray.length-1];
-    wordArray.pop();
-    $(this).html(wordArray.join(" "));
-  }
-});
