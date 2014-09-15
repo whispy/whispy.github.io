77870e7ac18db516b06015aaaa6c55ebda34ee38
Whispy <dan.berkowitz1@gmail.com>
2014-09-15 18:43:39 -0400

    vmousemove for mobile drag

--- haircut/custom.js
+++ haircut/custom.js
@@ -51,6 +51,43 @@ function sliderPreMove() {
 
 
     });
+
+$("div.pre").on("mousemove", (function(e) {
+        if (e.pageX >= minX && e.pageX <= maxX) {
+            var val = (e.pageX - minX) / tickSize;
+            console.log(val)
+            sliderPre.slider("value", val); 
+            if(val > 0 && val < 1){
+                $("img#pre").attr('src', 'images/PreHaircut-04.jpg')
+            }
+            if(val > 1 && val < 2){
+                $("img#pre").attr('src', 'images/PreHaircut-03.jpg')
+            }
+            if(val > 2 && val < 3){
+                $("img#pre").attr('src', 'images/PreHaircut-02.jpg')
+            }
+            if(val > 3 && val < 4){
+                $("img#pre").attr('src', 'images/PreHaircut-01.jpg')
+            }
+            if(val > 4 && val < 5){
+                $("img#pre").attr('src', 'images/PreHaircut-05.jpg')
+            }  
+            if(val > 5 && val < 6){
+                $("img#pre").attr('src', 'images/PreHaircut-06.jpg')
+            }
+            if(val > 6 && val < 7){
+                $("img#pre").attr('src', 'images/PreHaircut-07.jpg')
+            }
+            if(val > 7 && val < 8){
+                $("img#pre").attr('src', 'images/PreHaircut-08.jpg')
+            }
+            if(val > 8){
+                $("img#pre").attr('src', 'images/PreHaircut-09.jpg')
+            }    
+        }
+
+
+    }));
 };
 
 function sliderPostMove() {
@@ -104,6 +141,41 @@ function sliderPostMove() {
             }        
         }
     });
+
+ $("div.post").on("vmousemove", (function(e) {
+        if (e.pageX >= minX && e.pageX <= maxX) {
+            var val = (e.pageX - minX) / tickSize;
+            console.log(val)
+            sliderPost.slider("value", val); 
+            if(val > 0 && val < 1){
+                $("img#post").attr('src', 'images/PostHaircut-04.jpg')
+            }
+            if(val > 1 && val < 2){
+                $("img#post").attr('src', 'images/PostHaircut-03.jpg')
+            }
+            if(val > 2 && val < 3){
+                $("img#post").attr('src', 'images/PostHaircut-02.jpg')
+            }
+            if(val > 3 && val < 4){
+                $("img#post").attr('src', 'images/PostHaircut-01.jpg')
+            }
+            if(val > 4 && val < 5){
+                $("img#post").attr('src', 'images/PostHaircut-05.jpg')
+            }  
+            if(val > 5 && val < 6){
+                $("img#post").attr('src', 'images/PostHaircut-06.jpg')
+            }
+            if(val > 6 && val < 7){
+                $("img#post").attr('src', 'images/PostHaircut-07.jpg')
+            }
+            if(val > 7 && val < 8){
+                $("img#post").attr('src', 'images/PostHaircut-08.jpg')
+            }
+            if(val > 8){
+                $("img#post").attr('src', 'images/PostHaircut-09.jpg')
+            }        
+        }
+    }));
 };
 
 $(document).ready(function () {
