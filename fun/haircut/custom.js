6d44c0c6d9ed796f393af83099f79a005e270df7
Whispy <dan.berkowitz1@gmail.com>
2014-09-16 20:13:12 -0400

    updated js so that vmousemove is not run on non-touch devices, as it will lag if it does

--- fun/haircut/custom.js
+++ fun/haircut/custom.js
@@ -50,40 +50,42 @@ function sliderPreMove() {
         }
     });
 
-    $("div.pre").on("vmousemove", (function(e) {
-        if (e.pageX >= minX && e.pageX <= maxX) {
-            var val = (e.pageX - minX) / tickSize;
-            //console.log(val)
-            sliderPre.slider("value", val); 
-            if(val > 0 && val < 1){
-                $("img#pre").attr('src', 'images/PreHaircut-04.jpg')
+    if(hasTouch == true) {
+        $("div.pre").on("vmousemove", (function(e) {
+            if (e.pageX >= minX && e.pageX <= maxX) {
+                var val = (e.pageX - minX) / tickSize;
+                //console.log(val)
+                sliderPre.slider("value", val); 
+                if(val > 0 && val < 1){
+                    $("img#pre").attr('src', 'images/PreHaircut-04.jpg')
+                }
+                if(val > 1 && val < 2){
+                    $("img#pre").attr('src', 'images/PreHaircut-03.jpg')
+                }
+                if(val > 2 && val < 3){
+                    $("img#pre").attr('src', 'images/PreHaircut-02.jpg')
+                }
+                if(val > 3 && val < 4){
+                    $("img#pre").attr('src', 'images/PreHaircut-01.jpg')
+                }
+                if(val > 4 && val < 5){
+                    $("img#pre").attr('src', 'images/PreHaircut-05.jpg')
+                }  
+                if(val > 5 && val < 6){
+                    $("img#pre").attr('src', 'images/PreHaircut-06.jpg')
+                }
+                if(val > 6 && val < 7){
+                    $("img#pre").attr('src', 'images/PreHaircut-07.jpg')
+                }
+                if(val > 7 && val < 8){
+                    $("img#pre").attr('src', 'images/PreHaircut-08.jpg')
+                }
+                if(val > 8){
+                    $("img#pre").attr('src', 'images/PreHaircut-09.jpg')
+                }    
             }
-            if(val > 1 && val < 2){
-                $("img#pre").attr('src', 'images/PreHaircut-03.jpg')
-            }
-            if(val > 2 && val < 3){
-                $("img#pre").attr('src', 'images/PreHaircut-02.jpg')
-            }
-            if(val > 3 && val < 4){
-                $("img#pre").attr('src', 'images/PreHaircut-01.jpg')
-            }
-            if(val > 4 && val < 5){
-                $("img#pre").attr('src', 'images/PreHaircut-05.jpg')
-            }  
-            if(val > 5 && val < 6){
-                $("img#pre").attr('src', 'images/PreHaircut-06.jpg')
-            }
-            if(val > 6 && val < 7){
-                $("img#pre").attr('src', 'images/PreHaircut-07.jpg')
-            }
-            if(val > 7 && val < 8){
-                $("img#pre").attr('src', 'images/PreHaircut-08.jpg')
-            }
-            if(val > 8){
-                $("img#pre").attr('src', 'images/PreHaircut-09.jpg')
-            }    
-        }
-    }));
+        }));
+    }
 };
 
 function sliderPostMove() {
@@ -138,45 +140,61 @@ function sliderPostMove() {
         }
     });
 
- $("div.post").on("vmousemove", (function(e) {
-        if (e.pageX >= minX && e.pageX <= maxX) {
-            var val = (e.pageX - minX) / tickSize;
-            //console.log(val)
-            sliderPost.slider("value", val); 
-            if(val > 0 && val < 1){
-                $("img#post").attr('src', 'images/PostHaircut-04.jpg')
+    if(hasTouch == true) {
+        $("div.post").on("vmousemove", (function(e) {
+            if (e.pageX >= minX && e.pageX <= maxX) {
+                var val = (e.pageX - minX) / tickSize;
+                //console.log(val)
+                sliderPost.slider("value", val); 
+                if(val > 0 && val < 1){
+                    $("img#post").attr('src', 'images/PostHaircut-04.jpg')
+                }
+                if(val > 1 && val < 2){
+                    $("img#post").attr('src', 'images/PostHaircut-03.jpg')
+                }
+                if(val > 2 && val < 3){
+                    $("img#post").attr('src', 'images/PostHaircut-02.jpg')
+                }
+                if(val > 3 && val < 4){
+                    $("img#post").attr('src', 'images/PostHaircut-01.jpg')
+                }
+                if(val > 4 && val < 5){
+                    $("img#post").attr('src', 'images/PostHaircut-05.jpg')
+                }  
+                if(val > 5 && val < 6){
+                    $("img#post").attr('src', 'images/PostHaircut-06.jpg')
+                }
+                if(val > 6 && val < 7){
+                    $("img#post").attr('src', 'images/PostHaircut-07.jpg')
+                }
+                if(val > 7 && val < 8){
+                    $("img#post").attr('src', 'images/PostHaircut-08.jpg')
+                }
+                if(val > 8){
+                    $("img#post").attr('src', 'images/PostHaircut-09.jpg')
+                }        
             }
-            if(val > 1 && val < 2){
-                $("img#post").attr('src', 'images/PostHaircut-03.jpg')
-            }
-            if(val > 2 && val < 3){
-                $("img#post").attr('src', 'images/PostHaircut-02.jpg')
-            }
-            if(val > 3 && val < 4){
-                $("img#post").attr('src', 'images/PostHaircut-01.jpg')
-            }
-            if(val > 4 && val < 5){
-                $("img#post").attr('src', 'images/PostHaircut-05.jpg')
-            }  
-            if(val > 5 && val < 6){
-                $("img#post").attr('src', 'images/PostHaircut-06.jpg')
-            }
-            if(val > 6 && val < 7){
-                $("img#post").attr('src', 'images/PostHaircut-07.jpg')
-            }
-            if(val > 7 && val < 8){
-                $("img#post").attr('src', 'images/PostHaircut-08.jpg')
-            }
-            if(val > 8){
-                $("img#post").attr('src', 'images/PostHaircut-09.jpg')
-            }        
-        }
-    }));
+        }));
+    }
 };
 
 $(document).ready(function () {
 
-sliderPreMove();
-sliderPostMove();
+    hasTouch = undefined;
+    
+    window.addEventListener('touchstart', function setHasTouch () {
+        hasTouch = true;
+        console.log(hasTouch);
+        // Remove event listener once fired, otherwise it'll kill scrolling
+        // performance
+        window.removeEventListener('touchstart', setHasTouch);
+        sliderPreMove();
+        sliderPostMove();
+    }, false);
+
+
+    sliderPreMove();
+    sliderPostMove();
+    
 
 });
