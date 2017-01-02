532b861e8d53204634647bd310645f82d7f1a7bd
Dan Berkowitz <dan.berkowitz1+git@gmail.com>
2017-01-02 14:42:38 -0500

    updated grunt modules and gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+  require('load-grunt-tasks')(grunt);
+
 	grunt.initConfig({
 		includereplace: {
 			your_target: {
@@ -10,10 +12,18 @@ module.exports = function(grunt) {
       				{src: ['*.html', 'writings/*.html', 'designs/*.html'], dest: './', expand: true, cwd: 'grunt-templates/'}
       			]
     		}
-  		}
+  		},
+      imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'images/thumbs/',
+          src: ['*.{png,jpg,gif}'],
+          dest: 'images/thumbs/'
+        }]
+      }
+    }
 	})
 
-  grunt.loadNpmTasks('grunt-include-replace');
-
-  grunt.registerTask('default', ['includereplace']);
+  grunt.registerTask('default', ['includereplace','imagemin']);
 };
