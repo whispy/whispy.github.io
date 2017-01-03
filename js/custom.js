54aac3705c5b9e14cd8a886809e438dfdfa14cc4
Dan Berkowitz <dan.berkowitz1+git@gmail.com>
2017-01-02 19:28:14 -0500

    Make prefetching work on sidebar

--- js/custom.js
+++ js/custom.js
@@ -215,7 +215,7 @@ $(document).ready(function () {
 	fancyNav(curr_url);
 
 	//Enable Ajaxify.js on the listed elements
-	jQuery('#ajaxContent, #ajaxWork, #ajaxContent .sidebar-thumb, .index-thumbs-container .index-thumb, #selfieDiv, .navigation li').ajaxify({
+	jQuery('#ajaxContent, #ajaxWork, #ajaxContent .sidebar-thumb a, .index-thumbs-container .index-thumb, #selfieDiv, .navigation li').ajaxify({
 	});
 
 }); // $(document).ready end
