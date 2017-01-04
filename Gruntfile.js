dc2d5d2ebe901394c73d08e245cff1d782ef41bc
Dan Berkowitz <dan.berkowitz1+git@gmail.com>
2017-01-04 10:47:15 -0500

    update grunt to latest version

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,13 @@ module.exports = function(grunt) {
 			your_target: {
 				options: {
 				// Task-specific options go here.
-      			},
-      			files: [
-      				{src: ['*.html', 'writings/*.html', 'designs/*.html'], dest: './', expand: true, cwd: 'grunt-templates/'}
-      			]
-    		}
-  		},
-      imagemin: {
+      	},
+      	files: [
+      		{src: ['*.html', 'writings/*.html', 'designs/*.html'], dest: './', expand: true, cwd: 'grunt-templates/'}
+      	]
+    	}
+  	},
+    imagemin: {
       dynamic: {
         files: [{
           expand: true,
@@ -22,7 +22,42 @@ module.exports = function(grunt) {
           dest: 'images/thumbs/'
         }]
       }
-    }
+    },
+    postcss: {
+      app: {
+        options: {
+          map: true,
+          processors: [
+            require('autoprefixer')({browsers: 'last 3 versions'}),
+            require('postcss-flexibility')
+          ]
+        },
+        src: 'app_templates/skin_1/css/main.css',
+        dest: 'app_templates/skin_1/css/main.css'
+      },
+      landing: {
+        options: {
+          map: true,
+          processors: [
+            require('autoprefixer')({browsers: 'last 3 versions'}),
+            require('postcss-flexibility')
+          ]
+        },
+        src: 'app_templates/skin_1/landing/css/landing.css',
+        dest: 'app_templates/skin_1/landing/css/landing.css'
+      },
+      checkout: {
+        options: {
+          map: true,
+          processors: [
+            require('autoprefixer')({browsers: 'last 3 versions'}),
+            require('postcss-flexibility')
+          ]
+        },
+        src: 'app_templates/skin_1/checkout/css/checkout.css',
+        dest: 'app_templates/skin_1/checkout/css/checkout.css'
+      }
+    },
 	})
 
   grunt.registerTask('default', ['includereplace','newer:imagemin']);
